59614a257dfa80e9ab9dd117ed7f8eff6f216a51
Tyler Thompson <tyler.thompson@tufts.edu>
2022-12-02 15:01:31 -0500

    Cheeky client_id scraping

--- SongFetcher.py
+++ SongFetcher.py
@@ -5,15 +5,14 @@
 # Given a SoundCloud client ID, fetches 
 #
 
-import json
 import requests
 import os
 import dotenv
 from pydub import AudioSegment
+from urllib.parse import urlencode
 
 MAX_RETRIES = 10
 SONG_DIR = "songs"
-CLIENT_ID = dotenv.get_key(".env", "CLIENT_ID")
 
 # stubborn_get()
 # Given a url for a GET req, attempts up to MAX_RETRIES GET requests for
@@ -30,8 +29,14 @@ def stubborn_get(url):
 # given a search query and limit on max # of results to return, makes a GET
 # req to the SoundCloud API
 # returns a JSON response with elements:
-def search(query, limit=10):
-    url = f"https://api-v2.soundcloud.com/search/tracks?q={query}&client_id={CLIENT_ID}&limit={limit}"
+def search(query, limit=10, genre=""):
+    q_params = {
+        "q": query,
+        "limit": limit,
+        "filter.genre_or_tag": genre,
+        "client_id": get_client_id(),
+    }
+    url = f"https://api-v2.soundcloud.com/search/tracks?" + urlencode(q_params)
     response = stubborn_get(url)
     return response.json()["collection"]
 
@@ -55,7 +60,10 @@ def download_song(track):
         print(f"File {filename} already downloaded")
         return filename_wav
 
-    transcode_url = track["media"]["transcodings"][0]["url"] + "?client_id=" + CLIENT_ID
+    q_params = {
+        "client_id": get_client_id(),
+    }
+    transcode_url = track["media"]["transcodings"][0]["url"] + "?" + urlencode(q_params)
     response = stubborn_get(transcode_url)
     if response.status_code != 200:
         return None
@@ -85,3 +93,37 @@ def download_song(track):
     
     print("Done")
     return filename_wav
+
+def append_to_env(key, value):
+    with open(".env", "a") as f:
+        f.write(f"{key}={value}\n")
+
+# Attempts to retreive a SoundCloud client ID
+# First checks the .env file for a CLIENT_ID key
+# If that fails, it attempts to scrape a client_id from the SoundCloud website
+# If all fails, it will throw an exception
+def get_client_id():
+    # Try to get the client ID from the .env file
+    client_id = dotenv.get_key(".env", "CLIENT_ID")
+    if client_id:
+        return client_id
+    print("Trying to scrape client ID from SoundCloud")
+    
+    # Fall back to scraping the client ID from the SoundCloud website
+    res = requests.get("https://soundcloud.com")
+    # The link to the JS file with the client ID looks like this:
+	# <script crossorigin src="https://a-v2.sndcdn.com/assets/sdfhkjhsdkf.js"></script
+    urls = res.text.split("<script crossorigin src=\"")[1:]
+    if len(urls) == 0:
+        raise Exception("Could not find client ID")
+    # It seems like our desired URL is always imported last
+    target = urls[-1].split("\"></script>")[0]
+    # fetch js file
+    res = requests.get(target)
+    # find client id
+    if ",client_id:" in res.text:
+        client_id = res.text.split(",client_id:\"")[1].split("\"")[0]
+        print(f"Found client ID: {client_id}")
+        append_to_env("CLIENT_ID", client_id)
+        return client_id
+    raise Exception("Could not find client ID")
